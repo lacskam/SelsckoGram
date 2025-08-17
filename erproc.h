cb65b22658f6a638196f8e7d481cdcb54b79f646
Epilepsysx <Epilepsys4@gmail.com>
2025-08-17 13:56:06 +0300

    Wrapper func for syscalls

--- erproc.h
+++ erproc.h
@@ -6,11 +6,13 @@
 
 int Socket(int domain, int type,int protocol) ;
 
+void *Malloc(size_t size);
+
 void Bind(int sockFd,const struct sockaddr *addr, socklen_t addrlen) ;
 
 void Listen(int sockFd, int backlog) ;
-
-
+int Read(int sockFd,const struct sockaddr *addr, size_t len);
+int Write(int sockFd,const struct sockaddr *addr,size_t len);
 int Accept(int sockFd, struct sockaddr *addr, socklen_t *addrlen) ;
 void Connect(int sockFd,const struct sockaddr *addr, socklen_t addrlen);
 
--- proto.c
+++ proto.c
@@ -1,17 +1,23 @@
 #include "proto.h"
+#include "erproc.h"
 
 
+int send_packet(int fd, uint8_t type, const char* message) {
 
-void send_packet(int fd, uint8_t type, const char* message) {
-      uint32_t len = strlen(message);
+    uint32_t len = strlen(message);
 
-      packet* pkt = malloc(sizeof(packet) + len);
-      pkt->type = type;
-      pkt->payload_size = len;
-      memcpy(pkt->payload, message, len);
+    packet *pkt=Malloc(sizeof(packet)+len);
 
-      size_t total_size = sizeof(packet) + len;
-      ssize_t written = write(fd, pkt, total_size);
 
-      free(pkt);
+    pkt->type = type;
+    pkt->payload_size = len;
+    memcpy(pkt->payload, message, len);
+
+    size_t total_size = sizeof(packet) + len;
+    Write(fd,pkt,total_size);
+
+    free(pkt);
+    return 0;
+
 }
+
